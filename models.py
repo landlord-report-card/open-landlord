8b1905de6fc15fa70209ecec2797e4b09165fc0a
Andrew Kaier <akaier@Andrews-MacBook-Pro.local>
2022-08-23 19:19:03 -0400

    add additional fields to database and refactor populate script

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class Landlord(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     address = db.Column(db.String(250))
     location = db.Column(db.String(250))
+    property_count = db.Column(db.Integer)
 
 
     def __repr__(self):
@@ -21,12 +22,29 @@ class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     parcel_id = db.Column(db.String(250), nullable=False)
     address = db.Column(db.String(250), nullable=False)
+    zip_code = db.Column(db.String(10))
     property_type = db.Column(db.String(250))
     owner_id = db.Column(db.Integer, db.ForeignKey("landlord.id"))
     owner = db.relationship("Landlord", backref="properties")
     service_call_count = db.Column(db.Integer)
+    tenant_complaints = db.Column(db.Integer)
+    health_violation_count = db.Column(db.Integer)
+    eviction_probability = db.Column(db.Float)
+    court_case_count = db.Column(db.Integer)
+    deferred_maintenance_probability = db.Column(db.Float)
+    owner_occupied = db.Column(db.String(250))
+    rental_registry_count = db.Column(db.Integer)
+    inspection_count = db.Column(db.Integer)
+    rental_code_cases = db.Column(db.Integer)
+    code_violations_count = db.Column(db.Integer)
+    rent_plan_probability = db.Column(db.Float)
+    is_business = db.Column(db.String(250))
+    public_owner = db.Column(db.String(250))
+    business_entity_type = db.Column(db.String(250))
+    housing_instability_probability = db.Column(db.Float)
 
 
     def __repr__(self):
         return '<Property %r>' % self.address
+
 
--- populate_db.py
+++ populate_db.py
@@ -4,34 +4,95 @@ from models import db, Landlord, Property
 from app import app
 
 
+COLUMN_LIST = [
+    {"csv_column": "Parcel ID", "db_column": "parcel_id", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Tenant Complaint - Count by Source", "db_column": "tenant_complaints", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Health Violation - Count", "db_column": "health_violation_count", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Owner_1", "db_column": "name", "column_type":"STRING", "is_owner_col": True},
+    {"csv_column": "Eviction Probability", "db_column": "eviction_probability", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Court Case", "db_column": "court_case_count", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Calls for Service - Count", "db_column": "service_call_count", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Deferred Maintenance Probability", "db_column": "deferred_maintenance_probability", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Zip Code", "db_column": "zip_code", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Public Owner", "db_column": "public_owner", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Address", "db_column": "address", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Housing Instability Probability", "db_column": "housing_instability_probability", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Code Violations - Count", "db_column": "code_violations_count", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Owner Occupied", "db_column": "owner_occupied", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Rental Code Cases", "db_column": "rental_code_cases", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Rental Registry - Count by Rental Units & Status", "db_column": "rental_registry_count", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "OwnAddr_1", "db_column": "address", "column_type":"STRING", "is_owner_col": True},
+    {"csv_column": "Is Business", "db_column": "is_business", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Business Entity Type", "db_column": "business_entity_type", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Owner Property Count", "db_column": "property_count", "column_type":"STRING", "is_owner_col": True},
+    {"csv_column": "Rent Plan Probability", "db_column": "rent_plan_probability", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Inspection - Count", "db_column": "inspection_count", "column_type":"STRING", "is_owner_col": False},
+    {"csv_column": "Owner Location", "db_column": "location", "column_type":"STRING", "is_owner_col": True},
+]
+
+
 filename = sys.argv[1]
 
+
+def process_property(row, landlords, db):
+    # Hack to ignore "address unknown"
+    if "Address Unknown" in row["Address"]:
+        return
+
+    property_dict = {}
+    for column_obj in COLUMN_LIST:
+        if not column_obj["is_owner_col"]:
+
+            # Parse empty strings as nulls
+            clean_value = None if row[column_obj["csv_column"]] == "" else row[column_obj["csv_column"]]
+            property_dict[column_obj["db_column"]] = clean_value
+
+    # Include mapping to landlord table
+    property_dict["owner_id"] = landlords[row["Owner_1"]]
+    # print(landlords[row["Owner_1"]])
+
+    # if "parcel_id" not in property_dict:
+    #     print(property_dict)
+    #     print(row)
+
+    if property_dict["parcel_id"] is not None:
+        property_obj = Property(**property_dict)
+        db.session.add(property_obj)
+
+def process_landlord(row, landlords, db):
+    # If we've already processed this landlord, return
+    if row["Owner_1"] in landlords:
+        return
+
+    landlord_dict = {}
+    for column_obj in COLUMN_LIST:
+        if column_obj["is_owner_col"]:
+            landlord_dict[column_obj["db_column"]] = row[column_obj["csv_column"]]
+    landlord_obj = Landlord(**landlord_dict)
+    db.session.add(landlord_obj)
+    db.session.flush()
+    landlords[row["Owner_1"]] = landlord_obj.id
+
+
 with app.app_context():
+    db.create_all()
+    landlords = {}
+    # For convenience, we process all landlords first, store their IDs, then process properties
     with open(filename, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
-        landlords = {}
         for row in reader:
-            parcel_id = row['Parcel ID']
-            address = row['Address']
-            property_type = row['Property Type']
-            if row['Calls for Service - Count'] == "":
-                service_call_count = None
-            else:
-                service_call_count = row['Calls for Service - Count']
-            owner_location = row['Owner Location']
-            owner_name = row['Owner_1']
-            owner_address = row['OwnAddr_1']
+            process_landlord(row, landlords, db)
+    db.session.flush()
 
-            if owner_name not in landlords:
-                landlord_obj = Landlord(name=owner_name, address=owner_address, location=owner_location)
-                db.session.add(landlord_obj)
-                db.session.flush()
-                landlords[owner_name] = landlord_obj.id
-        
-            property_obj = Property(parcel_id=parcel_id, address=address, property_type=property_type, owner_id=landlords[owner_name], service_call_count=service_call_count)
-            db.session.add(property_obj)
+    print(landlords)
 
-        db.session.commit()
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            process_property(row, landlords, db)
+
+
+    db.session.commit()
 
 
 
