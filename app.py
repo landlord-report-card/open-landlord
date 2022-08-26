d874f5c0c0c327f8b19f3526b46e6e59dbe0578b
Andrew Kaier <akaier@Andrews-MacBook-Pro.local>
2022-08-26 11:59:43 -0400

    additional landlord fields

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, flash, request, redirect
+from sqlalchemy.sql import func
 from models import db, Landlord, Property
 from forms import LandlordSearchForm
 import os
@@ -22,11 +23,19 @@ def search_results(search):
     search_string = search.data['search']
     results = Property.query.filter(Property.address.ilike("%{}%".format(search_string))).join(Landlord, Landlord.id==Property.owner_id).add_columns(Landlord.name)
     return render_template('index.html', results=results, form=search)
-    
+
 
 @app.route('/landlord/<id>')
 def landlord(id):
-    landlord = Landlord.query.filter_by(id=id).first()
+    group_by_properties = (Landlord.name, Landlord.address, Landlord.location, Landlord.property_count)
+    landlord = Landlord.query.filter_by(id=id).with_entities(
+        func.sum(Property.service_call_count).label('service_call_count'),
+        func.sum(Property.code_violations_count).label('code_violations_count'),
+        func.sum(Property.tenant_complaints).label('tenant_complaints'),
+        func.sum(Property.health_violation_count).label('health_violation_count'),
+        func.sum(Property.rental_registry_count).label('rental_registry_count'),
+        func.sum(Property.rental_code_cases).label('rental_code_cases'), 
+        *group_by_properties).join(Property, Landlord.id==Property.owner_id).group_by(*group_by_properties).first()
     properties = Property.query.filter_by(owner_id=id)
     return render_template('landlord.html', landlord=landlord, properties=properties)
 
