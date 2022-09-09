a95d4df6409c60a9c4939109729fb1e8f74d5bae
Andrew Kaier <akaier@Andrews-MacBook-Pro.local>
2022-09-09 09:52:23 -0400

    add grading for substats

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template, flash, request, redirect
 from sqlalchemy.sql import func
 from models import db, Landlord, Property
 from forms import LandlordSearchForm
+from decimal import Decimal
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['LANDLORD_DATABASE_URI']
 db.init_app(app)
 
+
+GRADE_COLORS = {
+  'A': "2cba00",
+  'B': "a3ff00",
+  'C': "fff400",
+  'D': "ffa700",
+  'F': "ff0000",
+}
 
 PROGRESS_RED = "ff0000"
 PROGRESS_ORANGE = "ffa700"
@@ -23,9 +32,19 @@ GRADE_COMPONENTS = [
     "police_incidents",
 ]
 
-def add_coloring(stats, landlord):
+STATS_TO_SCALE = [
+    "tenant_complaints",
+    "code_violations",
+    "police_incidents",
+]
+
+def add_grade_and_color(stats, landlord):
     for component in GRADE_COMPONENTS:
-        stats["{}_color".format(component)] = get_stats_color(landlord[component], stats["average_{}".format(component)], stats["{}_std_dev".format(component)])
+        grade_and_color = get_stats_grade_and_color(landlord[component],
+                                                    stats["average_{}".format(component)], 
+                                                    stats["{}_std_dev".format(component)])
+        stats["{}_color".format(component)] = grade_and_color["color"]
+        stats["{}_grade".format(component)] = grade_and_color["grade"]
 
 
 def replace_none_with_zero(some_dict):
@@ -39,44 +58,54 @@ def get_std_devs(value, average, std_dev):
 def get_net_std_devs(stats, landlord):
     total = 0
     for component in GRADE_COMPONENTS:
-        total = total + get_std_devs(landlord[component], stats["average_{}".format(component)], stats["{}_std_dev".format(component)])
+        total = total + get_std_devs(landlord[component], 
+                                     stats["average_{}".format(component)], 
+                                     stats["{}_std_dev".format(component)])
     return total
+
+
+def get_grade_and_color_from_std_devs(std_devs):
+    if std_devs <= -2:
+        return {"grade":"F", "color":GRADE_COLORS["F"]}
+    elif std_devs <= -1:
+        return {"grade":"D", "color":GRADE_COLORS["D"]}
+    elif std_devs >= 2:
+        return {"grade":"A", "color":GRADE_COLORS["A"]}
+    elif std_devs >= 1:
+        return {"grade":"B", "color":GRADE_COLORS["B"]}
+    else:
+        return {"grade":"C", "color":GRADE_COLORS["C"]}
 
 
 def calculate_landlord_score(stats, landlord):
     std_devs = get_net_std_devs(stats, landlord)
-
-    if std_devs <= -2:
-        return {"grade":"F", "color":PROGRESS_RED}
-    elif std_devs <= -1:
-        return {"grade":"D", "color":PROGRESS_ORANGE}
-    elif std_devs >= 2:
-        return {"grade":"A", "color":PROGRESS_DARK_GREEN}
-    elif std_devs >= 1:
-        return {"grade":"B", "color":PROGRESS_LIGHT_GREEN}
-    else:
-        return {"grade":"C", "color":PROGRESS_YELLOW}
+    return get_grade_and_color_from_std_devs(std_devs)
 
 
-def get_stats_color(value, average, std_dev):
+def get_stats_grade_and_color(value, average, std_dev):
     if value is 0:
-        return PROGRESS_DARK_GREEN
+        return {"grade":"A", "color":GRADE_COLORS["A"]}
 
-    if (average - value) < (-2 * std_dev):
-        return PROGRESS_RED
-    elif (average - value) < (-1 * std_dev):
-        return PROGRESS_ORANGE
-    elif (average - value) > (2 * std_dev):
-        return PROGRESS_DARK_GREEN
-    elif (average - value) > (1 * std_dev):
-        return PROGRESS_LIGHT_GREEN
-    else:
-        return PROGRESS_ORANGE
+    std_devs = get_std_devs(value, average, std_dev)
+    grade_and_color = get_grade_and_color_from_std_devs(std_devs)
+    return grade_and_color
+
+
+def add_scaled_landlord_stats(stats):
+    for stat in STATS_TO_SCALE:
+        total_stat = stats["{}_total".format(stat)]
+        if total_stat is None:
+            stats[stat] = total_stat
+        else:
+            stats[stat] = Decimal(float(total_stat) / float(stats["property_count"]))
+    return stats
+
 
 def get_stats():
     ll_stats = get_landlord_stats()
     property_stats = get_property_stats()
     ll_stats.update(property_stats)
+
     return ll_stats
 
 def get_landlord_stats():
@@ -126,16 +155,18 @@ def landlord(id):
 
     landlord = Landlord.query.filter_by(id=id).with_entities(
         func.sum(Property.service_call_count).label('service_call_count'),
-        func.sum(Property.code_violations_count).label('code_violations'),
-        func.sum(Property.tenant_complaints).label('tenant_complaints'),
+        func.sum(Property.code_violations_count).label('code_violations_total'),
+        func.sum(Property.tenant_complaints).label('tenant_complaints_total'),
         func.sum(Property.health_violation_count).label('health_violation_count'),
-        func.sum(Property.police_incidents_count).label('police_incidents'), 
+        func.sum(Property.police_incidents_count).label('police_incidents_total'), 
         *group_by_properties).join(Property, Landlord.id==Property.owner_id).group_by(*group_by_properties).first()
+
+    landlord = add_scaled_landlord_stats(dict(landlord))
     properties = Property.query.filter_by(owner_id=id)
 
-    landlord = replace_none_with_zero(dict(landlord))
+    landlord = replace_none_with_zero(landlord)
 
-    add_coloring(stats, landlord)
+    add_grade_and_color(stats, landlord)
     landlord_score = calculate_landlord_score(stats, landlord)
 
     return render_template('landlord.html', landlord=landlord, properties=properties, 
