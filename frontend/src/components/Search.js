be00c8d190934ba997223eee479b06e4246b6db0
Andrew Kaier <akaier@Andrews-MacBook-Pro.local>
2022-11-02 13:47:42 -0400

    fix autocomplete issue

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -17,6 +17,9 @@ export default function Search() {
  
 
     React.useEffect(() => {
+        if (query.length === 0) {
+            return
+        }
         axios.get("/api/search?query=" + query).then((response) => {
           const addressResults = response.data
           const landlordIds = addressResults.map(a => a.owner_id);
@@ -40,7 +43,7 @@ export default function Search() {
     <div className="main">
     <h1>Find your landlord</h1>
     <p className="lead">Type in your address or landlord name to find your landlord!</p>
-    <input className="form-control me-2" id="search" type="text" onChange={event => setQuery(event.target.value)} />
+    <input className="form-control me-2" id="search" type="text" autoComplete="off" onChange={event => setQuery(event.target.value)} />
     <br />
     <Table data={data}>
       {(tableList) => (
